37f80b3f427c8fec37494c41c7a9133a8646d055
Azizul <azizul656589@gmail.com>
2021-10-09 15:23:03 +0600

    add more

---
+++ ISDB/jQuery/js/script.js
@@ -0,0 +1,18 @@
+//$(function(){
+	//$('#button').click(function(){
+		//$('div').hide();
+	//});
+//});
+
+//$(function(){
+	//$('#button').click(function(){
+		//$(this).hide();
+	//});
+//});
+
+
+$(function(){
+	$('button').click(function(){
+		$('ul li:nth-child(2)').hide();
+	});
+});
